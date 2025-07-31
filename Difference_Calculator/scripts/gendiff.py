075b293479af8434f948bc9813167bdfe48f5e2b
EggsellentGuy <ivanrossi112@gmail.com>
2025-07-31 15:32:27 +0300

    add lint

--- Difference_Calculator/scripts/gendiff.py
+++ Difference_Calculator/scripts/gendiff.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
 import argparse
+
 from Difference_Calculator.generate_diff import generate_diff
 
 
 def main():
     parser = argparse.ArgumentParser(
-            description="Compares two configuration files and shows a difference"
-        )
+    description=(
+        "Compares two configuration files "
+        "and shows a difference"
+    )
+)
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format', help="Set format of output")
